910855bcc62f7a4d16dbb86c0f877dc32f6691e2
Pludam <109984063+Pludam@users.noreply.github.com>
2025-03-16 20:52:09 +0100

    add basic ordering

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import simpy
 
 restaurant_env = simpy.Environment()
+order_store = simpy.Store(restaurant_env,10)
+order_inter = 7
 
-def cook(env):
+
+def add_order(env: simpy.Environment, order_store: simpy.Store):
+    order_i = 0
     while True:
+        order_i +=1
+        yield env.timeout(order_inter)
+        print(f"adding Order: {order_i} at {env.now}")
+        order_store.put(f"Order: {order_i}")
 
-        print('Cooking Order at %d' % env.now)
+    
 
-        parking_duration = 5
+def cook(env: simpy.Environment,order_store: simpy.Store):
+    while True:
+        
+        print('requesting Order at', env.now)
 
-        yield env.timeout(parking_duration)
+        item = yield order_store.get()
 
+        
+        print(f'Cooking {item} at {env.now}')
 
-        print('Getting Order at %d' % env.now)
+        cook_duration = 5
 
-        trip_duration = 2
+        yield env.timeout(cook_duration)
 
-        yield env.timeout(trip_duration)
+restaurant_env.process(add_order(restaurant_env,order_store))
+restaurant_env.process(cook(restaurant_env,order_store))
 
-restaurant_env.process(cook(restaurant_env))
 restaurant_env.run(until=15)
